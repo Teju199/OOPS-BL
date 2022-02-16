99c183b988b210e14f4be39f46f6b8fc21c5e0d2
Teju199 <tejuiyer95@gmail.com>
2022-02-16 17:13:23 +0530

    Person class added

---
+++ AddressBook/Person.java
@@ -0,0 +1,17 @@
+package AddressBook;
+
+public class Person {
+    private AddressBookUC2 addressBookUC2 = new AddressBookUC2();
+
+    public AddressBookUC2 getAddressBookUC2() {
+        return addressBookUC2;
+    }
+
+    public void setAddressBookUC2(AddressBookUC2 addressBookUC2) {
+        this.addressBookUC2 = addressBookUC2;
+    }
+
+
+
+}
+
